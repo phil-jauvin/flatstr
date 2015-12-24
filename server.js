638ad2b6fb3140ff812cdb8f7126cdafea59740f
Phil Jauvin <philippejauvin@gmail.com>
2015-12-24 12:59:33 -0500

    Merry christmas

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express = require('express');
+var mongoose = require('mongoose');
 var flats = require("./flats");
 
 var app = express();
 
+
 // Use html and css in public
 app.use(express.static(__dirname+"/public"));
-
 
 
 // Let's start defining some routes
@@ -16,12 +16,16 @@ app.get("/",function(req,res){
   res.sendFile(__dirname+"/public/index.html");
 });
 
+// Get all the flats from :page of Kijiji
 app.get("/flats/kijiji/:page",flats.kijiji);
+
 
 // "Page you're looking for can't be found"
 app.get("*",function(req,res){
   res.sendFile(__dirname+"/public/404.html");
 });
+
+
 
 var port = 3000;
 
