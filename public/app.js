d940dc48ac3596b49c0aa158d93e281dde68f1cc
Phil Jauvin <philippejauvin@gmail.com>
2015-12-28 17:11:56 -0500

    Added Angular GUI

--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
 var app = angular.module("flatstr",[]);
+
+
+app.controller("PageController",["$http",function($http){
+
+  var self = this;
+
+  $http.get("/flats/kijiji/111").then(function(response){
+    self.source = response.data.source;
+    self.flats = response.data.flats;
+  });
+
+}]);
+
+app.directive("flatList",function(){
+
+  return{
+    "restrict":"E",
+    "templateUrl":"flat-list.html"
+  };
+
+});
+
+app.directive("about",function(){
+
+  return{
+    "restrict":"E",
+    "templateUrl":"about.html"
+  };
+
+});
