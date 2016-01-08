6f633ea5ae97391ca096b0c9dfa1e29f9d7d9391
Phil Jauvin <philippejauvin@gmail.com>
2016-01-08 16:44:00 -0500

    Starting angular routing from scratch, Frontend was FUBAR

--- public/app.js
+++ public/app.js
@@ -1,46 +1,28 @@
-var app = angular.module("flatstr",["ng"]);
+var app = angular.module("flatstr",["ngResource","ngRoute"]);
 
 
-app.controller("PageController",["$http",function($http){
+app.controller("MainController",["$scope","$location","$log",function($scope,$location,$log){
 
-  self = this;
-  self.page = 1;
-  self.beds = 1;
-  self.baths = 1;
+  // Current tab in the navbar
+  $scope.tab = 0;
 
-  self.renderFlats = function(){
-
-    $http.get("/flats/kijiji/"+String(self.page)+String(self.beds)+String(self.baths)).then(function(response){
-      self.source = response.data.source;
-      self.flats = response.data.flats;
-    });
-
-  }
 }]);
 
-app.directive("flatList",function(){
+app.config(function($routeProvider){
 
-  return{
-    "restrict":"E",
-    "templateUrl":"flat-list.html"
-  };
+  $routeProvider.when("/",{
+    templateUrl:"test.html",
+    controller:"MainController"
+  });
 
-});
+  $routeProvider.when("/flats",{
+    templateUrl:"test.html",
+    controller:"MainController"
+  });
 
-app.directive("about",function(){
-
-  return{
-    "restrict":"E",
-    "templateUrl":"about.html"
-  };
-
-});
-
-app.directive("landingPage",function(){
-
-  return{
-    "restrict":"E",
-    "templateUrl":"landing.html"
-  };
+  $routeProvider.when("/about",{
+    templateUrl:"about.html",
+    controller:"MainController"
+  });
 
 });
